7c88ae34b3b782ede2936b0f39752d0722ff1c2d
Caroline Su <caroline77899@gmail.com>
2024-09-18 18:01:32 -0700

    add link redirect to signup page

--- card-game-client/app/login/page.tsx
+++ card-game-client/app/login/page.tsx
@@ -3,8 +3,12 @@ import LoginForm from "../components/ui/login-form";
 
 export default function Login() {
   return (
-    <div className="flex justify-center dark:bg-zinc-900 min-h-screen p-48">
-      <LoginForm />
-    </div>
+
+          <div className="flex flex-col justify-center items-center dark:bg-zinc-900 min-h-screen p-48">
+              <LoginForm/>
+              <a href="/signup"><p className="underline w-20" >Sign up</p></a>
+          </div>
+
+
   );
 }
