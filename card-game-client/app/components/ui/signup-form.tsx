083bf01dc139cf2b662b05e12a8b5ff6b5047ece
Caroline Su <caroline77899@gmail.com>
2024-09-18 21:35:28 -0700

    add function to post form

--- card-game-client/app/components/ui/signup-form.tsx
+++ card-game-client/app/components/ui/signup-form.tsx
@@ -1,48 +1,84 @@
-import React from "react";
+"use client";
+
+import React, { useState } from "react";
 import Card from "./card";
 import { Button } from "./button";
 
+
 export default function SignupForm() {
+  const [username, setUsername] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+
+    const userData = { username, email, password };
+
+    try {
+      const response = await fetch('/signup-submit', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/x-www-form-urlencoded',
+        },
+        body: new URLSearchParams(userData).toString(),
+      });
+
+      if (response.ok) {
+        const result = await response.json();
+        console.log('User created:', result);
+      } else {
+        const error = await response.json();
+        console.error('Error:', error);
+      }
+    } catch (error) {
+      console.error('Network Error:', error);
+    }
+    console.log(username, password);
+  };
   return (
-    <Card className="w-72 h-90 justify-center">
-      <form className="flex flex-col gap-4 h-full pt-2 w-full">
-        <div className="flex-grow flex flex-col gap-6">
-          <div className="flex flex-col gap-2">
-            <p>Username</p>
-            <input
-                id="username"
-                type="text"
-                placeholder="Username"
-                className="input border rounded-md w-full h-8 p-2 dark:bg-slate-200 dark:text-black"
-            />
+      <Card className="w-72 h-90 justify-center">
+        <form className="flex flex-col gap-4 h-full pt-2 w-full" onSubmit={handleSubmit}>
+          <div className="flex-grow flex flex-col gap-6">
+            <div className="flex flex-col gap-2">
+              <p>Username</p>
+              <input
+                  id="username"
+                  type="text"
+                  placeholder="Username"
+                  className="input border rounded-md w-full h-8 p-2 dark:bg-slate-200 dark:text-black"
+                  onChange={(e) => setUsername(e.target.value)}
+              />
+            </div>
+            <div className="flex flex-col gap-2">
+              <p>Email</p>
+              <input
+                  id="email"
+                  type="text"
+                  placeholder="Email"
+                  className="input border rounded-md w-full h-8 p-2 dark:bg-slate-200 dark:text-black"
+                  onChange={(e) => setEmail(e.target.value)}
+              />
+            </div>
+            <div className="flex flex-col gap-2">
+              <p>Password</p>
+              <input
+                  id="password"
+                  type="password"
+                  placeholder="Password"
+                  className="input border rounded-md w-full h-8 p-2 dark:bg-slate-200 dark:text-black"
+                  onChange={(e) => setPassword(e.target.value)}
+              />
+            </div>
           </div>
-          <div className="flex flex-col gap-2">
-            <p>Email</p>
-            <input
-                id="email"
-                type="text"
-                placeholder="Email"
-                className="input border rounded-md w-full h-8 p-2 dark:bg-slate-200 dark:text-black"
-            />
-          </div>
-          <div className="flex flex-col gap-2">
-            <p>Password</p>
-            <input
-                id="password"
-                type="password"
-                placeholder="Password"
-                className="input border rounded-md w-full h-8 p-2 dark:bg-slate-200 dark:text-black"
-            />
-          </div>
-        </div>
-        <Button
-            variant="outline"
-            type="submit"
-            className="bg-gray-800 hover:bg-gray-700 text-gray-100 hover:text-gray-100 border hover:border-gray-700"
-        >
-          Sign in
-        </Button>
-      </form>
-    </Card>
+          <Button
+              variant="outline"
+              type="submit"
+              className="bg-gray-800 hover:bg-gray-700 text-gray-100 hover:text-gray-100 border hover:border-gray-700"
+          >
+            Sign up
+          </Button>
+        </form>
+      </Card>
   );
-}
+} 
