b5ecdd8f38b4d387abd34c60a92bdb45734bf40b
Caroline Su <caroline77899@gmail.com>
2024-09-19 21:47:30 -0700

    feat: add POST action to submit button

--- card-game-client/app/components/ui/login-form.tsx
+++ card-game-client/app/components/ui/login-form.tsx
@@ -1,11 +1,41 @@
-import React from "react";
+"use client";
+import React, {useState} from "react";
 import Card from "./card";
 import { Button } from "./button";
 
 export default function LoginForm() {
+
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const handleLoginSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+
+    const userData = { email, password };
+
+    try {
+      const response = await fetch('/login-submit', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/x-www-form-urlencoded',
+        },
+        body: new URLSearchParams(userData).toString(),
+      });
+
+      if (response.ok) {
+
+        window.location.href = "/dashboard";
+        console.log('login successful');
+      } else {
+        const error = await response.json();
+        console.error('Error:', error);
+      }
+    } catch (error) {
+      console.error('Network Error:', error);
+    }
+  };
   return (
     <Card className="w-72 h-72 justify-center">
-      <form className="flex flex-col gap-4 h-full pt-2 w-full">
+      <form className="flex flex-col gap-4 h-full pt-2 w-full" onSubmit={handleLoginSubmit}>
         <div className="flex-grow flex flex-col gap-6">
           <div className="flex flex-col gap-2">
             <p>Email</p>
@@ -14,6 +44,7 @@ export default function LoginForm() {
               type="text"
               placeholder="Email"
               className="input border rounded-md w-full h-8 p-2 dark:bg-slate-200 dark:text-black"
+              onChange={(e)=>{setEmail(e.target.value)}}
             />
           </div>
           <div className="flex flex-col gap-2">
@@ -23,6 +54,7 @@ export default function LoginForm() {
               type="password"
               placeholder="Password"
               className="input border rounded-md w-full h-8 p-2 dark:bg-slate-200 dark:text-black"
+              onChange={(e)=>{setPassword(e.target.value)}}
             />
           </div>
         </div>
