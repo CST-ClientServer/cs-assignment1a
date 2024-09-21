9d22e067fba221c8801140e7a49b6a84d9e7ac5f
Jonathan <122506441+jwl0724@users.noreply.github.com>
2024-09-21 13:24:13 -0700

    [add] new upload url variable to utils

--- card-game-client/app/lib/utils.ts
+++ card-game-client/app/lib/utils.ts
@@ -8,3 +8,5 @@ export function cn(...inputs: ClassValue[]) {
 export const defaultImageUrl =
     "http://ec2-54-176-67-195.us-west-1.compute.amazonaws.com:8080/uploadFiles/c188b16a-0ce7-404c-8b2e-34129c8b9931_defaultImage.png";
 
+export const fileUploadUrl =
+    "http://ec2-54-176-67-195.us-west-1.compute.amazonaws.com:8080/uploadFiles/"
