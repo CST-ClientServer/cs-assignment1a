6375b35e22dcb274976fc107ed4d5d33f9e4b962
Jasper Oh <ohyj0906@gmail.com>
2024-09-28 22:55:52 -0700

    [Feat] Fix weird deployment process

--- card-game-client/app/gameRoom/page.tsx
+++ card-game-client/app/gameRoom/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import useWebSocket from "../hook/useWebSocket";
 
 export default function GameRoom() {
