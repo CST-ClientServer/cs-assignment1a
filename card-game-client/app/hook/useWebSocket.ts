c7a8b6589e663f1c340b94153abcdecd5edcea6b
Jasper Oh <ohyj0906@gmail.com>
2024-09-28 22:52:54 -0700

    [Feat] WebSocket BluePrint with chat functionality

---
+++ card-game-client/app/hook/useWebSocket.ts
@@ -0,0 +1,50 @@
+"use client";
+
+import { useEffect, useState } from 'react';
+
+type ChatMessage = {
+  message: string;
+};
+
+const useWebSocket = (url: string) => {
+  const [messages, setMessages] = useState<ChatMessage[]>([]);
+  const [ws, setWs] = useState<WebSocket | null>(null);
+  const [isConnected, setIsConnected] = useState<boolean>(false);
+
+  useEffect(() => {
+    const socket = new WebSocket(url);
+    setWs(socket);
+
+    socket.onopen = () => {
+      setIsConnected(true);
+    };
+
+    socket.onmessage = (event) => {
+      const data: ChatMessage = JSON.parse(event.data);
+      setMessages((prevMessages) => [...prevMessages, data]);
+    };
+
+    socket.onerror = (error) => {
+      console.error('WebSocket error:', error);
+    };
+
+    socket.onclose = () => {
+      setIsConnected(false);
+      console.log('WebSocket closed');
+    };
+
+    return () => {
+      socket.close();
+    };
+  }, [url]);
+
+  const sendMessage = (message: ChatMessage) => {
+    if (ws && isConnected) {
+      ws.send(JSON.stringify(message.message));
+    }
+  };
+
+  return { messages, sendMessage, isConnected };
+};
+
+export default useWebSocket;
